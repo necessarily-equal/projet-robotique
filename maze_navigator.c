3eebba3e1706b325d255eedb086dc3ef0b0ef4e2
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-05-14 14:24:34 +0200

    Refactor the maze navigator a bit

--- maze_navigator.c
+++ maze_navigator.c
@@ -20,33 +20,37 @@ static void dispatch_action(action_t action) {
 	}
 }
 
-static action_t find_next_action(void) {
-	static int count = 0;
-	if (count < 3) {
-		count++;
-		return ACTION_RIGHT;
+static void wait_action(action_t action) {
+	switch(action) {
+	case ACTION_STRAIGHT:
+		chBSemWait(get_edge_detected_semaphore_ptr());
+		break;
+	case ACTION_BACK:
+		chBSemWait(get_motor_semaphore_ptr());
+		break;
+	case ACTION_LEFT:
+		chBSemWait(get_motor_semaphore_ptr());
+		break;
+	case ACTION_RIGHT:
+		chBSemWait(get_motor_semaphore_ptr());
+		break;
 	}
+}
+
+static action_t find_next_action(void) {
+	// TODO
 	return ACTION_VOID;
 }
 
 void control_maze(void) {
-	action_queue_push(ACTION_LEFT);
-	action_queue_push(ACTION_LEFT);
-	action_queue_push(ACTION_LEFT);
 	while (1) {
-		action_t current_action = ACTION_VOID;
-		if (action_queue_empty()) {
+		action_t current_action = action_queue_pop();
+		if (!current_action) {
 			current_action = find_next_action();
 		}
 
-		if (!current_action) {
-			current_action = action_queue_pop();
-		}
-
-		if (current_action) {
-			saved_path_push(current_action);
-			dispatch_action(current_action);
-			chBSemWait(get_motor_semaphore_ptr());
-		}
+		saved_path_push(current_action);
+		dispatch_action(current_action);
+		wait_action(current_action);
 	}
 }
