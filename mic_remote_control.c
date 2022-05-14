76e6bbc0502d32c650e964610af171a8002fb6fa
Antoine Fontaine <antoine.fontaine@epfl.ch>
2022-05-14 16:07:58 +0200

    Cleanup and fix bug in mic_remote_control

--- mic_remote_control.c
+++ mic_remote_control.c
@@ -59,14 +59,8 @@
 
 //2 times FFT_SIZE because these arrays contain complex numbers
 static float micLeft_cmplx_input[2 * FFT_SIZE];
-//static float micRight_cmplx_input[2 * FFT_SIZE];
-//static float micFront_cmplx_input[2 * FFT_SIZE];
-//static float micBack_cmplx_input[2 * FFT_SIZE];
 //Arrays containing the computed magnitude of the complex numbers
 static float micLeft_output[FFT_SIZE];
-//static float micRight_output[FFT_SIZE];
-//static float micFront_output[FFT_SIZE];
-//static float micBack_output[FFT_SIZE];
 
 //Thread state
 static bool selector_thd_active = false;
@@ -154,59 +148,42 @@ void process_audio_data(int16_t *data, uint16_t num_samples){
 	static uint16_t nb_samples = 0;
 	static uint8_t mustSend = 0;
 
-	if(!disable_mic){
-		//loop to fill the buffers
-		for(uint16_t i = 0 ; i < num_samples ; i+=4){
-			//construct an array of complex numbers. Put 0 to the imaginary part
-			//micRight_cmplx_input[nb_samples] = (float)data[i + MIC_RIGHT];
-			micLeft_cmplx_input[nb_samples] = (float)data[i + MIC_LEFT];
-			//micBack_cmplx_input[nb_samples] = (float)data[i + MIC_BACK];
-			//micFront_cmplx_input[nb_samples] = (float)data[i + MIC_FRONT];
+	if(disable_mic)
+		return;
 
-			nb_samples++;
+	//loop to fill the buffers
+	for(uint16_t i = 0 ; i < num_samples ; i+=4){
+		//construct an array of complex numbers. Put 0 to the imaginary part
+		micLeft_cmplx_input[nb_samples++] = (float)data[i + MIC_LEFT];
+		micLeft_cmplx_input[nb_samples++] = 0;
 
-			//micRight_cmplx_input[nb_samples] = 0;
-			micLeft_cmplx_input[nb_samples] = 0;
-			//micBack_cmplx_input[nb_samples] = 0;
-			//micFront_cmplx_input[nb_samples] = 0;
-
-			//nb_samples++;
-
-			//stop when buffer is full
-			if(nb_samples >= (2 * FFT_SIZE)){
-				break;
-			}
-		}
-
+		//stop when buffer is full
 		if(nb_samples >= (2 * FFT_SIZE)){
-			/*	FFT proccessing
-			*
-			*	This FFT function stores the results in the input buffer given.
-			*	This is an "In Place" function. 
-			*/
-
-			//doFFT_optimized(FFT_SIZE, micRight_cmplx_input);
-			doFFT_optimized(FFT_SIZE, micLeft_cmplx_input, micLeft_output);
-			//doFFT_optimized(FFT_SIZE, micFront_cmplx_input);
-			//doFFT_optimized(FFT_SIZE, micBack_cmplx_input);
-
-			/*	Magnitude processing
-			*
-			*	Computes the magnitude of the complex numbers and
-			*	stores them in a buffer of FFT_SIZE because it only contains
-			*	real numbers.
-			*
-			*/
-			//arm_cmplx_mag_f32(micRight_cmplx_input, micRight_output, FFT_SIZE);
-			//arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
-			//arm_cmplx_mag_f32(micFront_cmplx_input, micFront_output, FFT_SIZE);
-			//arm_cmplx_mag_f32(micBack_cmplx_input, micBack_output, FFT_SIZE);
-
-			nb_samples = 0;
-			mustSend++;
-
-			mic_remote(micLeft_output);
+			break;
 		}
+	}
+
+	if(nb_samples >= (2 * FFT_SIZE)){
+		/*	FFT proccessing
+		*
+		*	This FFT function stores the results in the input buffer given.
+		*	This is an "In Place" function. 
+		*/
+
+		doFFT_optimized(FFT_SIZE, micLeft_cmplx_input, micLeft_output);
+
+		/*	Magnitude processing
+		*
+		*	Computes the magnitude of the complex numbers and
+		*	stores them in a buffer of FFT_SIZE because it only contains
+		*	real numbers.
+		*/
+		//arm_cmplx_mag_f32(micLeft_cmplx_input, micLeft_output, FFT_SIZE);
+
+		nb_samples = 0;
+		mustSend++;
+
+		mic_remote(micLeft_output);
 	}
 }
 
--- mic_remote_control.h
+++ mic_remote_control.h
@@ -6,19 +6,6 @@
 #ifndef _MOD_AUDIO_PROCESSING_H_
 #define _MOD_AUDIO_PROCESSING_H_
 
-typedef enum {
-    IDLE_CMD = 0,       // put the e-puck in mic idle
-    WAIT_CMD,           // prevents the e-puck from moving
-    MOVE_CMD,           // allows the e-puck to move
-    RST_MAZE_CMD,       // reset maze stored in memory
-    START_MAZE_CMD,     // start the maze navigation
-    U_TURN_CMD,         // order the e-puck to u turn (180deg)
-    TURN_LEFT_CMD,      // order the e-puck to turn left
-    TURN_RIGHT_CMD,     // order the e-puck to turn right
-    SLOW_DOWN_CMD,      // order the e-puck to go in slow speed
-    SPEED_UP_CMD,       // order the e-puck to go in "high" speed
-} command_t;
-
 void create_mic_selector_thd(void);
 
 void stop_mic_selector_thd(void);
