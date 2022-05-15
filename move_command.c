f0b4de99665e5ed837d47aee190a1f46e41e70ca
wagnieres-j <joel.wagnieres@epfl.ch>
2022-05-15 14:12:26 +0200

    fixed move cmd

--- move_command.c
+++ move_command.c
@@ -1,6 +1,7 @@
 /**
  * @file move_command.c
  * @brief 
+ * 
  */
 
 // C standard headers
@@ -8,56 +9,58 @@
 
 // ChibiOS headers
 #include "hal.h"
-//#include "ch.h"
+#include "ch.h"
 
 // e-puck 2 main processor headers
 #include <motors.h>
+#include "leds.h"
 
 // Module headers
+#include "ir_sensors.h"
 #include <move_command.h>
 
 /*===========================================================================*/
 /* Module constants.                                                         */
 /*===========================================================================*/
-
 //Geometric constants
 #define PI                  	3.1415926536f
 #define WHEEL_SEPARATION    	5.35f   // [cm]
 #define WHEEL_PERIMETER     	13      // [cm]
 #define EPUCK_PERIMETER     	(PI*WHEEL_SEPARATION)
-
 //Manoeuvre adjusted values based on experiments
 #define ADJUSTED_U_TURN			(EPUCK_PERIMETER/2)*0.98075
 #define ADJUSTED_90DEG_TURN 	ADJUSTED_U_TURN/2
-
 //Speed constants
 #define NULL_SPEED          0
 #define DEFAULT_SPEED		500
 #define MAX_SPEED			800		//Sufficent for our application
-
 //Steppers constants
 #define WHEEL_TURN_STEPS    1000    //Number of steps for one turn
-
 //Thread constants
 #define MOTOR_THD_PERIOD	100
 
-//Delay constants
-#define MOTOR_DELAY			100
+#define FRONT_WALL_THLD             1500 // IR1 & IR8
+#define FRONT_SIDE_WALL_THLD        1000 // IR2 & IR7
 
 /*===========================================================================*/
 /* Module local variables.                                                   */
 /*===========================================================================*/
 
-//Thread local variables
-static bool motor_thd_created = false;	//Thread is created
-static bool motor_thd_paused = false;	//Thread is paused
+static bool is_moving = false;
+static bool wall_ahead = false;
 
-static bool is_moving = false;			//Motor haven't reached target_pos
 static int32_t l_target_pos = 0;
 static int32_t r_target_pos = 0;
-static int16_t current_speed = DEFAULT_SPEED;
+static int32_t l_pos = 0;
+static int32_t r_pos = 0;
+static int32_t current_speed = DEFAULT_SPEED;
+
 static direction_t current_direction = FORWARD;
+static rotation_t current_rotation = COUNTERCLOCKWISE;
 static bool rotation_enabled = false;
+
+//Thread local variables
+static bool motor_thd_disabled = false;	//Thread is paused
 
 /*===========================================================================*/
 /* Semaphores.                                                               */
@@ -66,10 +69,49 @@ static bool rotation_enabled = false;
 static BSEMAPHORE_DECL(move_finished, TRUE);
 
 /*===========================================================================*/
-/* Module thread pointers                                                    */
+/* Module local functions.                                                   */
 /*===========================================================================*/
 
-static thread_t *ptr_motor_thd = NULL;
+bool collision_ahead(void) {
+	//Front checks
+    if(get_ir_delta(IR1) > FRONT_WALL_THLD) return true;
+    if(get_ir_delta(IR8) > FRONT_WALL_THLD) return true;
+    //Front left and right checks
+    if(get_ir_delta(IR2) > FRONT_SIDE_WALL_THLD) return true;
+    if(get_ir_delta(IR7) > FRONT_SIDE_WALL_THLD) return true;
+    return false;
+}
+
+void update_current_position(void) {
+	if (rotation_enabled) {
+		l_pos = current_rotation * left_motor_get_pos();
+		r_pos = current_rotation * right_motor_get_pos();
+	}
+	else {
+		l_pos = current_direction * left_motor_get_pos();
+		r_pos = current_direction * right_motor_get_pos();
+	}
+}
+
+bool position_reached(void) {
+	if (l_pos>l_target_pos && r_pos>r_target_pos && !rotation_enabled)
+		return true;
+	if (l_pos>l_target_pos && r_pos<r_target_pos && rotation_enabled)
+		return true;
+	return false;
+}
+
+void stop_moving(void) {
+	left_motor_set_speed(NULL_SPEED);
+	right_motor_set_speed(NULL_SPEED);
+	chBSemSignal(&move_finished);
+	current_direction = FORWARD;
+	current_rotation = COUNTERCLOCKWISE;
+	rotation_enabled = false;
+	l_target_pos = 0;
+	r_target_pos = 0;
+	is_moving = false;
+}
 
 /*===========================================================================*/
 /* Module threads.                                                           */
@@ -81,43 +123,18 @@ static THD_FUNCTION(motor_thd, arg) {
 	(void) arg;
 
 	systime_t time;
-	int32_t l_pos=0;
-	int32_t r_pos=0;
 
-	while (!chThdShouldTerminateX()) {
-		//Thread Sleep
-		chSysLock();
-		if (motor_thd_paused) {
-			left_motor_set_speed(NULL_SPEED);
-			right_motor_set_speed(NULL_SPEED);
-			chSchGoSleepS(CH_STATE_SUSPENDED);
+	while (true) {
+		wall_ahead = collision_ahead();
+		if (wall_ahead) stop_moving();
+		else {
+			update_current_position();
+			if (position_reached()) stop_moving();
 		}
-		chSysUnlock();
-		//Thread body
-		l_pos=current_direction * left_motor_get_pos();
-		r_pos=current_direction * right_motor_get_pos();
-
-		if (l_pos>l_target_pos && r_pos>r_target_pos && !rotation_enabled) {
-			left_motor_set_speed(NULL_SPEED);
-			right_motor_set_speed(NULL_SPEED);
-			is_moving = false;
-			chThdSleepMilliseconds(MOTOR_DELAY);
-			chBSemSignal(&move_finished);
-		}
-		else if (l_pos>l_target_pos && r_pos<r_target_pos && rotation_enabled) {
-			left_motor_set_speed(NULL_SPEED);
-			right_motor_set_speed(NULL_SPEED);
-			is_moving = false;
-			chThdSleepMilliseconds(MOTOR_DELAY);
-			chBSemSignal(&move_finished);
-		}
-
-		//Thread refresh rate
 		time = chVTGetSystemTime();
 		chThdSleepUntilWindowed(time, time + MS2ST(MOTOR_THD_PERIOD));
 	}
 
-	motor_thd_created = false;
 	chThdExit(0);
 }
 
@@ -125,44 +142,58 @@ static THD_FUNCTION(motor_thd, arg) {
 /* Module exported functions.                                                */
 /*===========================================================================*/
 
-void create_motor_thd(void) {
-	if (!motor_thd_created) {
-		motors_init();
-		ptr_motor_thd = chThdCreateStatic(wa_motor_thd,
-			sizeof(wa_motor_thd), NORMALPRIO, motor_thd, NULL);
-		motor_thd_created = true;
-	}
-}
-
-void stop_motor_thd(void) {
-	if (motor_thd_created) {
-		resume_motor_thd();
-		chThdTerminate(ptr_motor_thd);
-		chThdWait(ptr_motor_thd);
-		left_motor_set_speed(NULL_SPEED);
-		right_motor_set_speed(NULL_SPEED);
-		motor_thd_created = false;
-		motor_thd_paused = false;
-	}
+void init_motor_thd(void) {
+	motors_init();
+	chThdCreateStatic(wa_motor_thd, sizeof(wa_motor_thd), NORMALPRIO,
+					  motor_thd, NULL);
 }
 
 void pause_motor_thd(void) {
-	if (motor_thd_created) {
-		motor_thd_paused = true;
-	}
+	set_current_speed(NULL_SPEED);
+	motor_thd_disabled = true;
 }
 
 void resume_motor_thd(void) {
-	chSysLock();
-	if (motor_thd_created && motor_thd_paused) {
-		chSchWakeupS(ptr_motor_thd, CH_STATE_READY);
-		motor_thd_paused = false;
-	}
-	chSysUnlock();
+	set_default_speed();
+	motor_thd_disabled = false;
 }
 
-bool motor_is_moving(void) {
+bool motor_thd_status(void) {
+	return motor_thd_disabled;
+}
+
+bool get_is_moving(void) {
 	return is_moving;
+}
+
+void turn(float position, rotation_t direction) {
+	motor_thd_disabled = false;
+	if (!is_moving && !wall_ahead) {
+		left_motor_set_pos(0);
+		right_motor_set_pos(0);
+		l_target_pos = position * WHEEL_TURN_STEPS / WHEEL_PERIMETER;
+		r_target_pos = -(position * WHEEL_PERIMETER / WHEEL_PERIMETER);
+		left_motor_set_speed(direction * current_speed);
+		right_motor_set_speed(-(direction * current_speed));
+		current_rotation = direction;
+		rotation_enabled = true;
+		is_moving = true;
+	}
+}
+
+void move(float position, direction_t direction) {
+	motor_thd_disabled = false;
+	if (!is_moving && !wall_ahead) {
+		left_motor_set_pos(0);
+		right_motor_set_pos(0);
+		l_target_pos=position * WHEEL_TURN_STEPS / WHEEL_PERIMETER;
+		r_target_pos=position * WHEEL_PERIMETER / WHEEL_PERIMETER;
+		left_motor_set_speed(direction*current_speed);
+		right_motor_set_speed(direction*current_speed);
+		current_direction = direction;
+		rotation_enabled = false;
+		is_moving = true;
+	}
 }
 
 void right_angle_turn(direction_t direction) {
@@ -171,45 +202,6 @@ void right_angle_turn(direction_t direction) {
 
 void u_turn(void) {
 	turn(ADJUSTED_U_TURN, COUNTERCLOCKWISE);
-}
-
-void turn(float position, rotation_t direction) {
-	if (motor_thd_created && !motor_thd_paused) {
-		if (!is_moving) {
-			set_default_speed();
-			left_motor_set_pos(0);
-			right_motor_set_pos(0);
-			l_target_pos=position * WHEEL_TURN_STEPS / WHEEL_PERIMETER;
-			r_target_pos=-(position * WHEEL_PERIMETER / WHEEL_PERIMETER);
-
-			left_motor_set_speed(direction*DEFAULT_SPEED);
-			right_motor_set_speed(-(direction*DEFAULT_SPEED));
-			if(direction == CLOCKWISE)
-				current_direction = FORWARD;
-			else
-				current_direction = BACKWARD;
-			rotation_enabled = true;
-			is_moving = true;
-		}
-	}
-}
-
-void move(float position, direction_t direction) {
-	if (motor_thd_created && !motor_thd_paused) {
-		if (!is_moving) {
-			set_default_speed();
-			left_motor_set_pos(0);
-			right_motor_set_pos(0);
-			l_target_pos=position * WHEEL_TURN_STEPS / WHEEL_PERIMETER;
-			r_target_pos=position * WHEEL_PERIMETER / WHEEL_PERIMETER;
-
-			left_motor_set_speed(direction*DEFAULT_SPEED);
-			right_motor_set_speed(direction*DEFAULT_SPEED);
-			rotation_enabled = false;
-			current_direction = direction;
-			is_moving = true;
-		}
-	}
 }
 
 void set_default_speed(void) {
@@ -243,14 +235,6 @@ void set_lr_speed(int left_speed, int right_speed) {
 		right_motor_set_speed(-MAX_SPEED);
 	else
 		right_motor_set_speed(right_speed);
-}
-
-void stop_move(void) {
-	is_moving = false;
-	current_direction = FORWARD;
-	rotation_enabled = false;
-	l_target_pos = 0;
-	r_target_pos = 0;
 }
 
 binary_semaphore_t *get_motor_semaphore_ptr(void) {
--- move_command.h
+++ move_command.h
@@ -27,72 +27,26 @@ typedef enum {
 /*  External declarations                                                    */
 /*===========================================================================*/
 
-/**
- * @brief       Create the motor thread, this thread monitors the current
- *              current position of the motors with the target ones.
- * 
- */
-void create_motor_thd(void);
+void init_motor_thd(void);
 
-/**
- * @brief       Terminate the motor thread
- * 
- */
-void stop_motor_thd(void);
-
-/**
- * @brief       Put the motor thread into sleep, motor are set to null speed.
- * 
- */
 void pause_motor_thd(void);
-
-/**
- * @brief       Wake up the motor thread from sleep
- * 
- */
 void resume_motor_thd(void);
+bool motor_thd_status(void);
 
-/**
- * @brief       return true if the motors have not reached the previous
- *              specified target position.
- * 
- * @return true     position not reached and motors running
- * @return false    motors in idle and position reached
- */
-bool motor_is_moving(void);
+bool get_is_moving(void);
+
+void turn(float position, rotation_t direction);
+void move(float position, direction_t direction);
 
 void right_angle_turn(direction_t direction);
 void u_turn(void);
 
-void turn(float position, rotation_t direction);
-
-void move(float position, direction_t direction);
-
-/**
- * @brief Set the default speed (500 step/s)
- * 
- */
 void set_default_speed(void);
-
 void set_current_speed(int16_t new_speed);
-
 int16_t get_current_speed(void);
 
-/**
- * @brief manual override of the left/right speed for precise control
- * 
- * @param left_speed 
- * @param right_speed 
- */
 void set_lr_speed(int left_speed, int right_speed);
 
-void stop_move(void);
-
-/**
- * @brief       This semaphore tells you when the motor is done moving
- *              usefull for chBSemWait(get_motor_sempahore_ptr());
- * 
- * @return      binary_semaphore_t* 
- */
 binary_semaphore_t *get_motor_semaphore_ptr(void);
+
 #endif /* _MOVE_COMMAND_H_ */
